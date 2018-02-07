102ce1c32902d6b4053f7035199c19a677117424
Brannon Dorsey <brannon@brannondorsey.com>
2018-02-06 21:53:28 -0600

    sizeable overhaul

--- bin/create-csvs.py
+++ bin/create-csvs.py
@@ -139,16 +139,15 @@ def get_filetype(magic_sigs, hex_string):
 
 magic_sigs = load_magic_sigs()
 
-data = open_csv_writers('/media/bbpwn2/eMerge Drive/mftm-database/data/csv')
+data = open_csv_writers('/media/bbpwn2/emerge_drive/mftm-database/data/csv')
 
 blockchain = Blockchain(sys.argv[1])
 last_block_height = -1
-for block in blockchain.get_ordered_blocks(os.path.join(sys.argv[1], 'index')):
-
+for block, blk_file in blockchain.get_unordered_blocks(start_blockfile_basename='blk00980.dat'):
     # double check that ordered blocks are working
-    assert(block.height == last_block_height + 1)
+    # assert(block.height == last_block_height + 1)
     last_block_height = block.height
-
+    # print(block.height)
     # write the block info to blocks.csv
     block_header = block.header # cache the header getter
     data['writers']['blocks'].writerow({
@@ -157,144 +156,148 @@ for block in blockchain.get_ordered_blocks(os.path.join(sys.argv[1], 'index')):
         'timestamp_mined': block_header.timestamp
     })
 
-    try:
-        for tx_index, transaction in enumerate(block.transactions):
+    # try:
+    for tx_index, transaction in enumerate(block.transactions):
 
-            # write the transaction info to transaction.csv
-            data['writers']['transactions'].writerow({
-                'transaction_hash': transaction.hash,
-                'block_hash': block.hash,
-                'index': tx_index
-            })
+        # write the transaction info to transaction.csv
+        data['writers']['transactions'].writerow({
+            'transaction_hash': transaction.hash,
+            'block_hash': block.hash,
+            'index': tx_index
+        })
 
-            # if this is the first transaction in the block save its
-            # coinbase if it is in the ascii range
-            if tx_index == 0:
-                coinbase = transaction.inputs[0]
+        # if this is the first transaction in the block save its
+        # coinbase if it is in the ascii range
+        if tx_index == 0:
 
-                # Some coinbase scripts are not valid scripts
-                try:
-                    script_operations = coinbase.script.operations
+            coinbase = transaction.inputs[0]
 
-                    # An operation is a CScriptOP or pushed bytes
-                    for op_index, operation in enumerate(script_operations):
-                        if type(operation) == bytes and len(operation) > 3 \
-                                and is_ascii_text(operation):
-                            try:
-                                # try and decode the data as text
-                                coinbase_message = operation.decode('ascii')
-
-                                # write the coinbase message info to ascii_coinbase_messages.csv
-                                data['writers']['ascii_coinbase_messages'].writerow({
-                                    'transaction_hash': transaction.hash,
-                                    'script_op_index': op_index,
-                                    'data': coinbase_message,
-                                    'valid': 0,
-                                    'tags': '',
-                                    'bookmarked': 0,
-                                    'reviewed': 0,
-                                    'annotation': ''
-                                })
-
-                                print('[+] coinbase found in tx {} script index {}:'.format(transaction.hash, op_index))
-                                print(coinbase_message)
-
-                            except UnicodeDecodeError as err:
-                                pass
-                except CScriptInvalidError:
-                    pass
-
-            # write transaction input to transaction_inputs.csv
-            for input_index, _input in enumerate(transaction.inputs):
-                data['writers']['transaction_inputs'].writerow({
-                    'transaction_hash': transaction.hash,
-                    'index': input_index,
-                    'prev_output_transaction_hash': _input.transaction_hash,
-                    'prev_output_index': _input.transaction_index
-                })
-
-            text_buff = ''
-            bytes_buff = bytearray()
+            # Some coinbase scripts are not valid scripts
             try:
-                for output_index, output in enumerate(transaction.outputs):
-                    for address_index, address in enumerate(output.addresses):
+                script_operations = coinbase.script.operations
 
-                        data['writers']['transaction_outputs'].writerow({
-                            'transaction_hash': transaction.hash,
-                            'index': output_index,
-                            'address': address.address,
-                            'value': output.value
-                        })
-
-                        # address.address contains the base58 encoded address
-                        # this encoded value will always be in the ASCII range
-
-                        # we decode the base58 address
-                        # remove the 1st byte (which contains a 1 or a 3 to denote the sigtype)
-                        # remove the last 4 bytes because they are a checksum
-                        # and are left with 160 bits of binary data
-                        decodedBin = b58decode(address.address)
-                        decodedBin = decodedBin[1:-4]
-                        bytes_buff += decodedBin
-
+                # An operation is a CScriptOP or pushed bytes
+                for op_index, operation in enumerate(script_operations):
+                    if type(operation) == bytes and len(operation) > 3 \
+                            and is_ascii_text(operation):
                         try:
                             # try and decode the data as text
-                            text = decodedBin.decode()
-                            text_buff += text
+                            coinbase_message = operation.decode('ascii')
+
+                            # write the coinbase message info to ascii_coinbase_messages.csv
+                            data['writers']['ascii_coinbase_messages'].writerow({
+                                'transaction_hash': transaction.hash,
+                                'script_op_index': op_index,
+                                'data': coinbase_message,
+                                'valid': 0,
+                                'tags': '',
+                                'bookmarked': 0,
+                                'reviewed': 0,
+                                'annotation': ''
+                            })
+
+                            print('[+] coinbase found in tx {} script index {}:'.format(transaction.hash, op_index))
+                            print(coinbase_message)
+
                         except UnicodeDecodeError as err:
                             pass
+            except CScriptInvalidError:
+                pass
 
-                        # if this is the last address in the last output
-                        if output_index == len(transaction.outputs) - 1 and \
-                        address_index == len(output.addresses) - 1:
+        # write transaction input to transaction_inputs.csv
+        for input_index, _input in enumerate(transaction.inputs):
+            data['writers']['transaction_inputs'].writerow({
+                'transaction_hash': transaction.hash,
+                'index': input_index,
+                'prev_output_transaction_hash': _input.transaction_hash,
+                'prev_output_index': _input.transaction_index
+            })
 
-                            filetype = get_filetype(magic_sigs, bytes_buff.hex())
-                            if filetype:
-                                # transaction_hash ; data ; filetype ; valid ; tags ; bookmarked ; reviewed ; annotation
-                                # write the coinbase message info to ascii_coinbase_messages.csv
-                                data['writers']['file_address_messages'].writerow({
-                                    'transaction_hash': transaction.hash,
-                                    # save binary data as a hex string
-                                    'data': bytes_buff.hex(),
-                                    'filetype': filetype,
-                                    'valid': 0,
-                                    'tags': '',
-                                    'bookmarked': 0,
-                                    'reviewed': 0,
-                                    'annotation': ''
-                                })
-                                # print('---------------------------------------------')
-                                print('[+] possible "{}" file found in tx {}'.format(filetype, transaction.hash))
+        if _input.script.is_return():
+            print('FOUND SCRIPT OP_RETURN IN TX: {}'.format(transaction.hash))
 
-                            # if text was found in any of the output address blocks
-                            if text_buff != '':
-                                # transaction_hash ; data ; filetype ; valid ; tags ; bookmarked ; reviewed ; annotation
-                                # write the coinbase message info to ascii_coinbase_messages.csv
-                                data['writers']['utf8_address_messages'].writerow({
-                                    'transaction_hash': transaction.hash,
-                                    # save utf8 data as a hex string
-                                    'data': binascii.hexlify(bytes(text_buff, encoding='utf8')).decode(),
-                                    'filetype': '',
-                                    'valid': 0,
-                                    'tags': '',
-                                    'bookmarked': 0,
-                                    'reviewed': 0,
-                                    'annotation': ''
-                                })
+        text_buff = ''
+        bytes_buff = bytearray()
+        try:
+            for output_index, output in enumerate(transaction.outputs):
+                for address_index, address in enumerate(output.addresses):
 
-                                # print('---------------------------------------------')
-                                # print('length: {}'.format(len(text_buff)))
-                                # print('bytes: {}'.format("".join("{:02x}".format(ord(c)) for c in text_buff)))
-                                print('[+] utf8 data found in tx {}:'.format(transaction.hash))
-                                print('{}'.format(text_buff))
-                                text_buff = ''
+                    data['writers']['transaction_outputs'].writerow({
+                        'transaction_hash': transaction.hash,
+                        'index': output_index,
+                        'address': address.address,
+                        'value': output.value
+                    })
 
-            except CScriptTruncatedPushDataError as err:
-                print('[!] caught: {}'.format(err), file=sys.stderr)
-    except AssertionError as err:
-        print('[!] caught: {}'.format(err), file=sys.stderr)
+                    # address.address contains the base58 encoded address
+                    # this encoded value will always be in the ASCII range
 
-    if block.height % 1000 == 0:
+                    # we decode the base58 address
+                    # remove the 1st byte (which contains a 1 or a 3 to denote the sigtype)
+                    # remove the last 4 bytes because they are a checksum
+                    # and are left with 160 bits of binary data
+                    decodedBin = b58decode(address.address)
+                    decodedBin = decodedBin[1:-4]
+                    bytes_buff += decodedBin
+
+                    try:
+                        # try and decode the data as text
+                        text = decodedBin.decode()
+                        text_buff += text
+                    except UnicodeDecodeError as err:
+                        pass
+
+                    # if this is the last address in the last output
+                    if output_index == len(transaction.outputs) - 1 and \
+                    address_index == len(output.addresses) - 1:
+
+                        filetype = get_filetype(magic_sigs, bytes_buff.hex())
+                        if filetype:
+                            # transaction_hash ; data ; filetype ; valid ; tags ; bookmarked ; reviewed ; annotation
+                            # write the coinbase message info to ascii_coinbase_messages.csv
+                            data['writers']['file_address_messages'].writerow({
+                                'transaction_hash': transaction.hash,
+                                # save binary data as a hex string
+                                'data': bytes_buff.hex(),
+                                'filetype': filetype,
+                                'valid': 0,
+                                'tags': '',
+                                'bookmarked': 0,
+                                'reviewed': 0,
+                                'annotation': ''
+                            })
+                            # print('---------------------------------------------')
+                            print('[+] possible "{}" file found in tx {}'.format(filetype, transaction.hash))
+
+                        # if text was found in any of the output address blocks
+                        if text_buff != '':
+                            # transaction_hash ; data ; filetype ; valid ; tags ; bookmarked ; reviewed ; annotation
+                            # write the coinbase message info to ascii_coinbase_messages.csv
+                            data['writers']['utf8_address_messages'].writerow({
+                                'transaction_hash': transaction.hash,
+                                # save utf8 data as a hex string
+                                'data': binascii.hexlify(bytes(text_buff, encoding='utf8')).decode(),
+                                'filetype': '',
+                                'valid': 0,
+                                'tags': '',
+                                'bookmarked': 0,
+                                'reviewed': 0,
+                                'annotation': ''
+                            })
+
+                            # print('---------------------------------------------')
+                            # print('length: {}'.format(len(text_buff)))
+                            # print('bytes: {}'.format("".join("{:02x}".format(ord(c)) for c in text_buff)))
+                            print('[+] utf8 data found in tx {}:'.format(transaction.hash))
+                            print('{}'.format(text_buff))
+                            text_buff = ''
+
+        except CScriptTruncatedPushDataError as err:
+            print('[!] caught 1: {}'.format(err), file=sys.stderr)
+    # except AssertionError as err:
+    #     print('[!] caught AssertionError in block #{}'.format(block.hash), file=sys.stderr)
+
+    if block.height and block.height % 1000 == 0:
 
         # flush files after each 1000 blocks, because fuuuuuuuuuu for not
         data['files']['blocks'].flush()
